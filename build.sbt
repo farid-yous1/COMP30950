b49a137571bd17ad2106eae803be6da55d231edd
Farid-Yous <farid.yous05@gmail.com>
2025-10-20 20:43:06 +0100

    Final commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+ThisBuild / scalaVersion := "3.3.7"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+name := "assignment1_MPP"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.18" % Test
--- src/main/scala/Student.scala
+++ src/main/scala/Student.scala
@@ -1,1 +1,4 @@
-final case class Student(name: String, courseNames: List[String], college: College)
+final case class Student(name: String, courseNames: List[String], college: College){
+  override def toString: String =
+    "name: " + name + ", courses: " + courseNames + ", college: " + college
+}
